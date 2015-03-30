a9b457370d655ec313a2f87980eb09877b62146e
Boris Fritscher <boris@fritscher.ch>
2015-03-30 10:59:12 +0200

    move footer down

--- templates/_runnings.js
+++ templates/_runnings.js
@@ -8,6 +8,6 @@ exports.header = {
 exports.footer = {
     height: '2cm',
     contents: function(pageNum, numPages) {
-      return '<div style="font-family: <%= font %>;margin:0 0.8cm;"><div style="display:inline-block;height: 100%;vertical-align: bottom;width:0px;"></div><div style="display:inline-block;text-align:center;width:100%;position:relative"><span style="text-align:left;position:absolute;left:0;bottom:0;"><%= bottomLeft %></span>' + pageNum + ' / ' + numPages + '<img style="height: 1cm;vertical-align:bottom;position:absolute;bottom:0;right:0" src="<%=assetDir%>/common/logo_hes-so_noir.jpg" /></div></div>';
+      return '<div style="font-family: <%= font %>;padding-top: 0.3cm;margin: 0 0.8cm 0 0.8cm;"><div style="display:inline-block;height: 100%;vertical-align: bottom;width:0px;"></div><div style="display:inline-block;text-align:center;width:100%;position:relative"><span style="text-align:left;position:absolute;left:0;bottom:0;"><%= bottomLeft %></span>' + pageNum + ' / ' + numPages + '<img style="height: 1cm;vertical-align:bottom;position:absolute;bottom:0;right:0" src="<%=assetDir%>/common/logo_hes-so_noir.jpg" /></div></div>';
     }
   }
