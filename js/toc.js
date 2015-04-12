9ca1716405c7c372aa66e79d997fe17ab0466cfd
Boris Fritscher <boris@fritscher.ch>
2015-04-13 00:05:19 +0200

    nicer toc

--- js/toc.js
+++ js/toc.js
@@ -2,24 +2,45 @@
 'use strict';
   var $ul = jQuery('<ul></ul>');
   
-  jQuery('h1, h2, h3').each(function(i, item){
-    if(item.innerText.trim() !== ''){
-        jQuery('<li>' + item.innerText + '</li>')
-        .appendTo($ul).on('click', function(){
-          var x, y = 0;
-          var $item = jQuery(item);
-          if($item.parent().parent().hasClass('slides')){
-            x = $item.parent().index();
-            y = 0;
-          }else{
-            x = $item.parent().parent().index();
-            y = $item.parent().index();
-          }
-          Reveal.slide(x, y);
-          Reveal.toggleOverview();
-        });
+  jQuery('.slides > section').each(function(x, item){
+    var subsections = jQuery(item).find('section');
+    if(subsections.length > 0){
+      subsections.each(function(y, item){
+        createSectionEntry(item, x, y);
+      });
+    }else{
+      createSectionEntry(item, x, 0);
     }
   });
+  
+  function createSectionEntry(item, x, y){
+    var $li = jQuery('<li></li>');
+    if(y > 0){
+      $li.addClass('subsection');
+    }
+    $li.appendTo($ul).on('click', function(){
+      Reveal.slide(x, y);
+      Reveal.toggleOverview();
+    });
+    Reveal.addEventListener( 'slidechanged', function( event ) {
+      $li.toggleClass('active', event.indexh === x && event.indexv === y);
+    });
+    
+    var title = jQuery(item).find('h1, h2, h3, h4, p').first();
+    var text = '';
+    if(title){
+      $li.addClass(title.prop('tagName'));
+      text = title.text().trim();
+      if(text === ''){
+        text = x + '-' + y;
+      }
+      if(text.length > 54){
+        text = text.substr(0, 54) + '...';
+      }
+    }
+    $li.text(text);
+  }
+  
   var $toc = jQuery('<div id="toc" class="toc"></div>');
   $toc.appendTo(jQuery('.reveal'));
   var $button = jQuery('<button id="tocshow">TOC</button>');
