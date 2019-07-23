b733230a7549ea134700e561b9fbc153f0ecfd3f
Boris Fritscher <boris@fritscher.ch>
2019-07-23 12:12:50 +0200

    support domtree-preview in addition of html-preview

--- js/htmlPreview.js
+++ js/htmlPreview.js
@@ -11,6 +11,22 @@
       }).blur();
       jQuery(item).before($preview);
     });
+
+    jQuery('.domtree-preview').each(function(x, item) {
+      const $preview = jQuery('<iframe class="preview-domtree" src="https://fritscher.ch/dom-css/"></iframe>');
+      const iframe = $preview[0];
+      $preview.addClass(Object.values(item.classList).filter(name => name.startsWith('output-')).map(name => name.slice(7)).join(' '));
+      const $code = jQuery(item.children[0]);
+      $code.on('blur keyup paste input', function() {
+        if (iframe.contentWindow) {
+          iframe.contentWindow.postMessage({ html: $code.text()}, '*');
+        }
+      }).blur();
+      iframe.onload = function() {
+        $code.blur();
+      };
+      jQuery(item).before($preview);
+    });
   }
 
   Reveal.addEventListener('ready', function() {
